b0ec595c217aad0ebedf6ab45f3a87febadfe0b1
Christian Simeon <simeonchristianjames@gmail.com>
2026-02-12 16:02:33 +0800

    Update proposal message and details

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -175,8 +175,8 @@ export default function ValentinesProposal() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            Thank you for accepting, I love you! 💕
-            <p className="text-sm mt-4">For more information, write me!!! 💌</p>
+             Yeheyyy!!!, I love you! 💕
+            <p className="text-sm mt-4">Date tayo sa D'KING sa 14, See you there! Mwaaa!❤️</p>
             <Image
               src="/hamster_jumping.gif"
               alt="Hamster Feliz"
